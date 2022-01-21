d63cc7f0438f974a35bfc578629baa9d59d2bd84
Alexis Petrovich <alexispetrovich11@gmail.com>
2022-01-21 11:19:47 -0300

    add router Login

--- 06-heroes-app/src/routers/AppRouter.js
+++ 06-heroes-app/src/routers/AppRouter.js
@@ -1,24 +1,19 @@
 //Router Principal
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { DcScreen } from "../components/dc/DcScreen";
 import { LoginScreen } from "../components/login/LoginScreen";
-import { MarvelScreen } from "../components/marvel/MarvelScreen";
-import { SearchScreen } from "../components/search/SearchScreen";
-import { Navbar } from "../components/ui/Navbar";
+import { DashboardRoutes } from "./DashboardRoutes";
+
 
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Navbar></Navbar>
+
       <Routes>
 
-        <Route path="/" element={<MarvelScreen />} />
-        <Route path="/marvel" element={<MarvelScreen />} />
-        <Route path="/dc" element={<DcScreen />} />
-        <Route path="/search" element={<SearchScreen />} />
         <Route path="/login" element={<LoginScreen />} />
+        
+        <Route path="/*" element={<DashboardRoutes />} /> 
         
       </Routes>
     </BrowserRouter>
