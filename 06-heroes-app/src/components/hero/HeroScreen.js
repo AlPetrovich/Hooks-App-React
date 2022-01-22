08c2aca43490abbc33715e95a245004589152c43
Alexis Petrovich <alexispetrovich11@gmail.com>
2022-01-21 23:01:41 -0300

    add queryString, Navigate, Location

--- 06-heroes-app/src/components/hero/HeroScreen.js
+++ 06-heroes-app/src/components/hero/HeroScreen.js
@@ -1,22 +1,58 @@
-import { useParams, Navigate } from "react-router-dom";
+import { useMemo } from "react";
+import { useParams, Navigate, useNavigate } from "react-router-dom";
 import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
 
-  const {heroId} = useParams();
+  const { heroId } = useParams();
+  const navigate = useNavigate();
 
-  const hero = getHeroById(heroId);
+  const handleReturn = () => {
+     navigate(-1); //navega a la pagina anterior
+  }
+
+  const hero = useMemo( ()=> getHeroById(heroId), [ heroId ]);
   
   if(!hero){
     return <Navigate to='/'></Navigate>
   }
 
+  //desestructurar todo del hero
+  const{
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters
+  }= hero;
+
+  const imagePath = `/assets/heroes/${ id }.jpg`;
+
   return(
-    <div>
-        <h1>HeroScreen</h1>
-        <p>
-          { hero.superhero }
-        </p>
+    <div className="row mt-5">
+       <div className="col-4">
+         <img src={ imagePath } alt={ superhero } className="img-thumbnail animate__animated animate__fadeInLeft"></img>
+       </div>
+       <div className="col-8">
+          <h3>{ superhero }</h3>
+
+          <ul className="list-group list-group-flush animate__animated animate__fadeIn">
+              <li className="list-group-item"><b>Alter ego:</b>{ alter_ego }</li>
+              <li className="list-group-item"><b>Publisher:</b>{ publisher }</li>
+              <li className="list-group-item"><b>first_appearance</b>{ first_appearance }</li>
+          </ul>
+
+          <h5 className=" mt-3">Characters</h5>
+          <p>{ characters }</p>
+      
+          <button 
+            className="btn btn-outline-info"
+            onClick={ handleReturn }
+          >
+            Regresar
+          </button>
+       </div>
     </div>
   );
 };
--- 06-heroes-app/src/components/search/SearchScreen.js
+++ 06-heroes-app/src/components/search/SearchScreen.js
@@ -1,10 +1,78 @@
-import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import { useForm } from '../../hooks/useForm';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
+import { HeroCard } from '../hero/HeroCard';
 
 export const SearchScreen = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    //console.log(location.search)
+    const { q = ''} = queryString.parse(location.search);
+   
+
+    const [value, handleInputChange ] = useForm({
+        searchText: q,
+    });
+    const { searchText } = value;
+
+    const heroesFilted = getHeroesByName(searchText);
+   
+    const handleSearch = (e) => {
+        e.preventDefault();
+        navigate(`?=${ searchText }`);
+    }
+
   return (
       
         <div>
-            <h1>SearchScreen</h1>
+            <h1>Busquedas</h1>
+            <hr></hr>
+
+
+            <div className="row">
+                <div className="col-5">
+                    <h4>Buscar</h4>
+                    <hr></hr>
+
+                    <form onSubmit={ handleSearch }>
+                        <input  
+                        type="text"
+                        placeholder="Buscar un heroe"
+                        className="form-control"
+                        name="searchText"
+                        autoComplete="off"
+                        onChange={ handleInputChange }
+                        value={ searchText }
+                        ></input>
+
+                        <button 
+                        className="btn btn-outline-primary mt-1"
+                        type="submit"
+                        >
+                            Buscar...
+                        </button>
+
+                        
+                    </form>
+                </div>
+                <div 
+                className="col-7"
+                >
+                    <h4>Resultados</h4>
+                    <hr></hr>
+                    {
+                        heroesFilted.map(hero=>(
+                            <HeroCard  
+                                key={hero.id}
+                                {...hero}
+                            />
+                        ))
+                    }
+
+                </div>
+            </div>
         </div>
 
     );
