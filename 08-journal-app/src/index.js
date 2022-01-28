9531649501efecb6050e2ebf31c2c84dbba07389
Alexis Petrovich <alexispetrovich11@gmail.com>
2022-01-28 10:53:38 -0300

    styles login/register

---
+++ 08-journal-app/src/index.js
@@ -0,0 +1,12 @@
+import  ReactDOM  from 'react-dom';
+import { JournalApp } from './JournalApp';
+import './styles/styles.scss';
+
+
+
+ReactDOM.render(
+
+    <JournalApp />,
+  document.getElementById('root')
+);
+
