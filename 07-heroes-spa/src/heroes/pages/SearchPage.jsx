bc396a43ba997db67787d121d97d68049cdb188a
alpetrovich <alexispetrovich11@gmail.com>
2022-08-11 10:59:20 -0300

    showError & showSearch

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -12,16 +12,16 @@ export const SearchPage = () => {
   const {q = ''} = queryString.parse(location.search);
   const heroes = getHeroesByName(q);
 
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
     searchText: q
   });
   
   const onSearchSubmit = (e) => {
     e.preventDefault();
-    if(searchText.trim().length <= 1) return;
-
     navigate(`?q=${searchText}`);
-
   }
 
   return (
@@ -51,14 +51,22 @@ export const SearchPage = () => {
           <div className="col-7">
               <h4>Results</h4>
               <hr/>
+              {/* {
+                (q === '') 
+                ? <div className="alert alert-primary">Search a hero</div>
+                : (heroes.length === 0) && <div className="alert alert-danger">No hero with <b>{ q }</b></div>
+              } */}
 
-              <div className="alert alert-primary">
+              <div className="alert alert-primary animate__animated animate__fadeIn" 
+                  style={{display: showSearch ? '' : 'none'}}>
                 Search a hero
               </div>
 
-              <div className="alert alert-danger">
+              <div className="alert alert-danger animate__animated animate__fadeIn" 
+                  style={{display: showError? '' : 'none'}}>
                 No hero with <b>{ q }</b>
               </div>
+              
               {
                 heroes.map(hero =>(
                   <HeroCard key={hero.id} {...hero} />
