c225343fdd45f03af1b81aafc8303acc05bc6b28
alpetrovich <alexispetrovich11@gmail.com>
2022-08-11 09:55:05 -0300

    getById, memorizo id publisher

--- 07-heroes-spa/src/heroes/routes/HeroesRoutes.jsx
+++ 07-heroes-spa/src/heroes/routes/HeroesRoutes.jsx
@@ -2,7 +2,6 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import { Navbar } from "../../ui/components/NavBar"
 import { DcPage, MarvelPage, HeroPage, SearchPage } from "../pages"
 
-
 export const HeroesRoutes = () => {
   return (
     <div>
@@ -14,7 +13,7 @@ export const HeroesRoutes = () => {
                 <Route path="marvel" element={<MarvelPage />} />
                 <Route path="dc" element={<DcPage />} />
                 <Route path="search" element={<SearchPage />}/>
-                <Route path="hero" element={<HeroPage />} />
+                <Route path="hero/:id" element={<HeroPage />} />
                 <Route path="/" element={<Navigate to="/marvel"/>} />
 
             </Routes>
