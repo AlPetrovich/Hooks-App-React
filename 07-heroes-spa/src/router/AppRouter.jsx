a9a5e6903bea276625df911db1672ede000497a5
alpetrovich <alexispetrovich11@gmail.com>
2022-08-12 11:22:52 -0300

    rutas privadas, publicas, autenticacion basica

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,16 +1,28 @@
 import { Routes, Route} from "react-router-dom"
 import { LoginPage } from "../auth/pages/LoginPage"
 import { HeroesRoutes } from "../heroes"
+import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute"
 
 export const AppRouter = () => {
   return (
     <div>
 
         <Routes>
-        
-            <Route path="login" element={<LoginPage />} />
 
-            <Route path="/*" element={<HeroesRoutes />} />
+            <Route path="login" element={
+              <PublicRoute>
+                <LoginPage />
+              </PublicRoute>
+            }
+            />
+            
+            <Route path="/*" element={
+                <PrivateRoute>
+                    <HeroesRoutes />
+                </PrivateRoute>
+              } 
+            />
 
         </Routes>
     </div>
