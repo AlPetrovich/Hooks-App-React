2a2572c6e40ecc837e0c5707ba8ba58e14a66f54
alpetrovich <alexispetrovich11@gmail.com>
2022-08-17 11:58:57 -0300

    [add] thunks y axios

--- 09-toolkit-redux/src/PokemonApp.jsx
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -1,6 +1,16 @@
-
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getPokemos } from "./store/slices/pokemon";
 
 export const PokemonApp = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch( getPokemos() )
+  }, [])
+  
+
   return (
     <div>
         <h1>Pokemon App</h1>
